d31702ea28a8fd3f2f6c02bada8fbbb4f99504d8
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-31 01:12:33 -0200

    Extremes and Elements modifications and Monoids

--- src/Statistics.hs
+++ src/Statistics.hs
@@ -1,16 +1,26 @@
 module Statistics where
 
 
-
-
-
-type MinElem = Double
-type MaxElem = Double
 type AccElems = Double
 type LengthElems = Integer
 type Elems = [Double]
 
-data Elements = Elements Elems LengthElems AccElems MaxElem MinElem
+type MinElem = Double
+type MaxElem = Double
+
+data Extremes = Ex MinElem MaxElem | Inv deriving (Eq, Show)
+
+instance Monoid Extremes where
+  mempty = Inv
+  mappend (Ex a b) (Ex a' b') = Ex (min a a') (max b b')
+  mappend (Ex a b) _ = Ex a b
+  mappend _  (Ex a b) = Ex a b
+
+
+
+data Elements = Elements Elems LengthElems AccElems Extremes
+  deriving (Eq, Show)
+
 
 type Average = Double
 type AccAverage = Double
@@ -19,3 +29,9 @@ type Deviation = Double
 
 data HurstDataType =
    HDT Elements Average AccAverage TresholdRange Deviation
+
+
+instance Monoid Elements where
+  mempty = Elements [] 0 0 Inv
+  mappend (Elements es l a e) (Elements es' l' a' e') =
+    Elements (es `mappend` es') (l + l') (a + a') (e `mappend` e')
