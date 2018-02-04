e96f0baae9713e68e2370ff3ab73f5acfbc929c8
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-02-04 15:32:55 -0200

    dependencies and default

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import MHLib
+import qualified Data.ByteString.Lazy as BL
+import Control.Applicative
+import Data.Csv
+import Data.Time
+
+newtype DefaultToZero = DefaultToZero Double
+
+
+instance FromField DefaultToZero where
+  parseField s = case runParser (parseField s) of
+    (Left _) -> pure $ DefaultToZero 0
+    (Right n) -> pure $ DefaultToZero n
+
+
+-- data Quotation = { date }
+
+
+
 
 main :: IO ()
 main = putStrLn "Here begins"
