eae3c7f1dd9843db257d771f3c26246f99a92915
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-02-10 20:51:59 -0200

    extractor

--- src/MHCharts.hs
+++ src/MHCharts.hs
@@ -3,21 +3,21 @@ module MHCharts where
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo 
 import Data.Time
-import MHDataTypes
-
--- data RenderQuotation =  R enderQuotation Day Point
-
-
-testPoints :: Num a => [(a, a)] 
-testPoints = [(1,1),(2,2),(3,3)]
-
-
+import qualified MHDataTypes as D
 
 signal :: [Double] -> [(Double,Double)]
 signal xs = [ (x,x) | x <- xs ]
 
 pts :: Double -> [Double] -> [(Double,Double)]
 pts f xs = [ (x,f) | x <- xs]
+
+
+extractPoint :: D.HurstDataType -> (Double, Double)
+extractPoint (D.HDT _ _ _  (D.Point (a,b)) ) = (a,b)
+
+extractPoints :: [D.HurstDataType] -> [[(Double, Double)]]
+extractPoints xs = [ps]
+  where ps = map (\x -> extractPoint x)  xs
 
 
 main = toFile def "example.png" $ do
